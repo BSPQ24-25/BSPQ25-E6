7ae751e7f395700f632043f6ed8efcf18d396a79
Diego Ramirez <diegoramirez@opendeusto.es>
2025-03-26 11:00:38 +0100

    SPRING BOOT PROJECT

---
+++ src/test/java/BSPQ25_E6/taskmanager/TaskmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package BSPQ25_E6.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
