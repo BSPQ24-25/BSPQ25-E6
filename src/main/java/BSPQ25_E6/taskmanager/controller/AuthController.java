256a44b3e4918a8667487db1edabbd21304bf92a
jonmendizabal <jonmendizabal@localhost>
2025-04-01 03:45:24 +0200

    Now users can signup and login persistently
    Db configured and made it persistent.

--- src/main/java/BSPQ25_E6/taskmanager/controller/AuthController.java
+++ src/main/java/BSPQ25_E6/taskmanager/controller/AuthController.java
@@ -1,5 +1,10 @@
 package BSPQ25_E6.taskmanager.controller;
+import BSPQ25_E6.taskmanager.model.User;
+import BSPQ25_E6.taskmanager.repository.UserRepository;
+import java.util.Optional;
 
+import org.apache.logging.log4j.CloseableThreadContext.Instance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,38 +14,41 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class AuthController 
 {
-
+	
+	@Autowired
+	private UserRepository userRepository;
+	
     //Login page 
     @GetMapping("/login")
     public String showLoginForm(Model model) 
     {
         return "login";
     }
-/* 
+
     //login form controller
     @PostMapping("/login")
     public String loginUser(@RequestParam String email,
                             @RequestParam String password,
-                            Model model) 
-                            {
-        //Spring 2 
-        if (email.equals("test@test.com") && password.equals("1234")) 
-        {
-            return "redirect:/dashboard";
-        } else 
-        {
-            model.addAttribute("error", "Password invalid");
-            return "login";
+                            Model model
+                           ) {
+        Optional<User> userOpt = userRepository.findByEmail(email);
+        if (userOpt.isPresent()) {
+            User user = userOpt.get();
+            if (password.equals(user.getPassword())) {
+            	
+                
+                return "redirect:/dashboard";
+            }
         }
+        model.addAttribute("error", "Credenciales inválidas");
+        return "login";
     }
-*/
     //Signup page
     @GetMapping("/signup")
     public String showSignupForm(Model model) 
     {
         return "signup";
     }
-/* 
     //signup form controller
     @PostMapping("/signup")
     public String registerUser(@RequestParam String username,
@@ -48,9 +56,26 @@ public class AuthController
                                @RequestParam String password,
                                Model model) 
                                {
-        //spring 2 
-        model.addAttribute("success", "User register");
+    	if (userRepository.findByEmail(email).isPresent()) {
+            model.addAttribute("error", "The email is already registered.");
+            return "signup";
+        }
+
+        User newUser = new User();
+        newUser.setUsername(username);
+        newUser.setEmail(email);
+        newUser.setPassword(password);
+        userRepository.save(newUser);
+
+        model.addAttribute("success", "User registered succesfully, please log in.");
         return "login";
+    
     }
-*/
+    @GetMapping("/logout")
+    public String logout( ) {
+        userSession.setLoggedUser(null); 
+        return "redirect:/login"; 
+    }
+
+
 }
