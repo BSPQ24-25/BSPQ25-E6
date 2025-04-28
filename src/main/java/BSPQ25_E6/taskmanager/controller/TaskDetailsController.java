4481fad7b8a2cf3448eaf3ca1af4b2e7c032745b
Iker Cubillo <i.cubillo@opendeusto.es>
2025-04-28 09:19:29 +0200

    Working on TaskDetails

--- src/main/java/BSPQ25_E6/taskmanager/controller/TaskDetailsController.java
+++ src/main/java/BSPQ25_E6/taskmanager/controller/TaskDetailsController.java
@@ -1,13 +1,23 @@
 package BSPQ25_E6.taskmanager.controller;
 
+import BSPQ25_E6.taskmanager.model.Task;
+import BSPQ25_E6.taskmanager.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class TaskDetailsController {
 
-    @GetMapping("/taskDetail")
-    public String taskDetail() {
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping("/taskDetail/{id}")
+    public String taskDetail(@PathVariable("id") Long taskId, Model model) {
+        Task task = taskService.findTaskById(taskId);
+        model.addAttribute("task", task);
         return "taskDetail";
     }
 }
---
+++ src/main/java/BSPQ25_E6/taskmanager/service/TaskService.java
@@ -0,0 +1,17 @@
+package BSPQ25_E6.taskmanager.service;
+
+import BSPQ25_E6.taskmanager.model.Task;
+import BSPQ25_E6.taskmanager.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public Task findTaskById(Long taskId) {
+        return taskRepository.findById(taskId).orElseThrow(() -> new RuntimeException("Task not found"));
+    }
+}
